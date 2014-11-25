cd6e70fc80737462453ddbfcdc8e96597b834b96
Terrence Wong <wongterrencew@gmail.com>
2014-11-24 21:23:28 -0500

    starter file in Perl

---
+++ starter.pl
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+
+#Sample code to read in test cases:
+#open(INFILE, "<", $ARGV[0]) or die("Cannot open file $ARGV[0] for reading: $!");
+#while(my $line = <INFILE>) {
+#    next if($line =~ m/^\s$/);  # skip blank lines
+#    $line =~ s/(^\s|\s*$)//g;   # remove leading and trailing spaces
+#    ... do something with this line
+#}
+#close(INFILE);
